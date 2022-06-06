72c9a3b713f6686c628d181bb044981f2c466459
ToxyFlog1627 <toxymail1627@gmail.com>
2022-06-06 13:29:00 +0300

    colors

--- config.h
+++ config.h
@@ -90,44 +90,38 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#6E6C7E",
+	"#F28FAD",
+	"#ABE9B3",
+	"#FAE3B0",
+	"#96CDFB",
+	"#F5C2E7",
+	"#89DCEB",
+	"#D9E0EE",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#988BA2",
+	"#F28FAD",
+	"#ABE9B3",
+	"#FAE3B0",
+	"#96CDFB",
+	"#F5C2E7",
+	"#89DCEB",
+	"#D9E0EE",
 
-	[255] = 0,
+	[256] = "#D9E0EE", /* default foreground colour */
+	[257] = "#1E1E2E", /* default background colour */
+	[258] = "#C3BAC6", /*575268*/
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#c6d0f5",
-	"#51576d",
-	"#dfdfff", /* default foreground colour */
-	"#232634", /* default background colour */
 };
 
-
 /*
- * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
-
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;
 /*
  * Default shape of cursor
  * 2: Block ("█")
